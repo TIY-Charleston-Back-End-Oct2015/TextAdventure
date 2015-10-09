bf1506f8638336a95cd6af21c06b686661f481b2
oakes <zsoakes@gmail.com>
2015-10-09 11:14:43 -0400

    Start command system

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by zach on 10/8/15.
  */
@@ -9,5 +11,19 @@ public class Game {
         player.chooseName();
         player.chooseWeapon();
         player.chooseArea();
+    }
+
+    static String nextLine() {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        if (s.startsWith("/")) {
+            if (s.equals("/help")) {
+                System.out.println("These are the available commands:");
+                System.out.println("/help => List available commands.");
+            }
+            return nextLine();
+        } else {
+            return s;
+        }
     }
 }
