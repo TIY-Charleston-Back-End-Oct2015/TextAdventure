81d7960a1d3b85751ee740fa69f521bb23c29e54
oakes <zsoakes@gmail.com>
2015-10-12 10:12:07 -0400

    Add main method

--- src/Game.java
+++ src/Game.java
@@ -4,14 +4,17 @@ import java.util.Scanner;
  * Created by zach on 10/8/15.
  */
 public class Game {
-    public static void run() throws Exception {
+    static Player player;
+
+    public static void main(String[] args) throws Exception {
         System.out.println("Welcome to my text adventure.");
+        player = new Player();
 
         while (true) {
-            Player player = new Player();
             player.chooseName();
             player.chooseWeapon();
             player.chooseArea();
+            player.findItem("Shield");
         }
     }
 
@@ -24,6 +27,13 @@ public class Game {
                 System.out.println("/help => List available commands.");
             } else if (s.equals("/exit")) {
                 System.exit(0);
+            } else if (s.equals("/inv")) {
+                if (player.items.size() == 0) {
+                    System.out.println("You have no items");
+                }
+                for (Object item : player.items) {
+                    System.out.println(item);
+                }
             }
             return nextLine();
         } else {
