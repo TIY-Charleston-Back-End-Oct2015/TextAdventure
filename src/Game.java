7b25e096d0cec72a57152a62cca542e35678c242
oakes <zsoakes@gmail.com>
2015-10-15 10:51:02 -0400

    Add constant and getters

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,9 @@
+import jodd.json.JsonParser;
+import jodd.json.JsonSerializer;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.Scanner;
 
 /**
@@ -5,15 +11,19 @@ import java.util.Scanner;
  */
 public class Game {
     static Player player;
+    static final String FILE_NAME = "save.json";
 
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome to my text adventure.");
-        player = new Player();
+        player = loadGame();
 
-        player.chooseName();
-        player.chooseWeapon();
-        player.chooseArea();
-        player.findItem("Shield");
+        if (player == null) {
+            player = new Player();
+            player.chooseName();
+            player.chooseWeapon();
+            player.chooseArea();
+            player.findItem("Shield");
+        }
 
         Weapon ogreWeapon = new Weapon();
         ogreWeapon.name = "Club";
@@ -21,6 +31,8 @@ public class Game {
         Enemy ogre = new Enemy("Ogre", 50, 5, ogreWeapon);
 
         player.battle(ogre);
+
+        saveGame();
     }
 
     static String nextLine() {
@@ -43,6 +55,35 @@ public class Game {
             return nextLine();
         } else {
             return s;
+        }
+    }
+
+    static void saveGame() {
+        File f = new File(FILE_NAME);
+        JsonSerializer serializer = new JsonSerializer();
+        String contentToSave = serializer.serialize(player);
+
+        try {
+            FileWriter fw = new FileWriter(f);
+            fw.write(contentToSave);
+            fw.close();
+        } catch (Exception e) {
+
+        }
+    }
+
+    static Player loadGame() {
+        try {
+            File f = new File(FILE_NAME);
+            FileReader fr = new FileReader(f);
+            int fileSize = (int) f.length();
+            char[] contents = new char[fileSize];
+            fr.read(contents);
+            String fileContents = new String(contents);
+            JsonParser parser = new JsonParser();
+            return parser.parse(fileContents, Player.class);
+        } catch (Exception e) {
+            return null;
         }
     }
 }
